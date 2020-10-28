97d4826c7277cb2022b9acd0d4979251e85004ba
Dan Watson <67798284+D4N-W4TS0N@users.noreply.github.com>
2020-10-28 09:25:48 +0000

    Add files via upload

---
+++ input.py
@@ -0,0 +1,6 @@
+first = float(input("First number: "))
+second = float(input("Second number: "))
+
+sum = first + second
+
+print("sum: " + str(sum))
---
+++ variables.py
@@ -0,0 +1,4 @@
+first_name = "Dan Watson"
+age = "twelve"
+male = True
+print("Hello, I am " + first_name + " my age is " + age + ".")
