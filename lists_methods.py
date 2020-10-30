bd465c4e1addc691a0541e1dfee2442abb71e60e
Dan Watson <67798284+D4N-W4TS0N@users.noreply.github.com>
2020-10-30 09:15:23 +0000

    Add files via upload

---
+++ lists_methods.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5] #declares a list
+print(1 in numbers) #checks if "1" is in the list
+print(len(numbers))
+numbers.insert(0,-1) #inserts list item at any place
+numbers.append(6) #adds any list item
+print(numbers)
+numbers.remove(3) #removes any list item
+print(numbers)
---
+++ tuples.py
@@ -0,0 +1,3 @@
+numbers = (1, 2, 3, 3)
+numbers.count(3) #counts how many 3's
+numbers.index(2) #returns the first occurence of the given value
