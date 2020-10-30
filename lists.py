398591b68877ebf314a154b36346ef0da881dbc1
Dan Watson <67798284+D4N-W4TS0N@users.noreply.github.com>
2020-10-30 08:44:26 +0000

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,5 @@
+names = ["John", "Bob", "Mosh", "Sam", "Mary"]
+print(names)
+names[0] = "Jon" #changes the list item
+print(names[0:3]) #prints all list items from 0-3
+print(names[4]) #prints 4th list item
---
+++ while_loops.py
@@ -0,0 +1,4 @@
+i = 1
+while i <= 10:
+    print(i * "this is a while loop")
+    i = i + 1
